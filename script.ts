c320b8df0a4d22f3a239f9428249a5a8f0e76737
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-04-27 15:54:07 +0200

    Add Surge call API

--- script.ts
+++ script.ts
@@ -1,28 +1,46 @@
-interface Post {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-}
+type GraphQLQuery = string;
+type Variables = { [key: string]: any };
 
-const fetchPosts = async (): Promise<Post[]> => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+const endpoint = "https://api.thegraph.com/subgraphs/name/somemoecoding/surgeswap-v2";
+const tokenID = process.argv[2] || "0x9f19c8e321bD14345b797d43E01f0eED030F5Bff";
+
+const query = `
+    query SurgeSwapV2TokenDayDatas($id: ID!) {
+        token(id: $id) {
+            tokenDayData(where: {_change_block: {number_gte: 0}}) {
+                date
+                dailyVolumeUSD
+                priceUSD
+                totalLiquidityUSD
+            }
+        }
+    }
+`;
+
+const fetchData = async (endpoint: string, query: GraphQLQuery, variables: Variables) => {
+    const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer YOUR_ACCESS_TOKEN", // If the API requires an access token, add it here.
+        },
+        body: JSON.stringify({ query, variables }),
+    });
 
     if (!response.ok) {
-        throw new Error(`An error occurred: ${response.statusText}`);
+        throw new Error(`Network response was not ok: ${response.status}`);
     }
 
-    const data: Post[] = await response.json();
-    return data;
-}
+    return await response.json();
+};
 
 const main = async () => {
     try {
-        const posts = await fetchPosts();
-        console.log(posts);
+        const result = await fetchData(endpoint, query, { id: tokenID });
+        console.log("Get data", result.data.token.tokenDayData);
     } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error("Error fetching data:", error);
     }
-}
+};
 
 main();
